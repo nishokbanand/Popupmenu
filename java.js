6efeb7330a5aade7e2ada7b3b7418af73d388edd
Nishok Anand B <nishokbanand@gmail.com>
2020-07-06 20:04:20 +0530

    changes done

---
+++ java.js
@@ -0,0 +1,21 @@
+let openbutton = document.querySelector(".openbutton");
+
+let closebutton = document.querySelector(".closebutton");
+
+let modal = document.querySelector(".modal");
+
+
+
+openbutton.addEventListener("click",function(){
+    modal.style.display = "block";
+})
+
+closebutton.addEventListener("click",function() {
+    modal.style.display="none"
+})
+
+window.addEventListener("click",function(e){
+    if(e.target == modal){
+        modal.style.display="none"
+    }
+})
